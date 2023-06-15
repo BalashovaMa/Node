60dd0419574397129c08183890d6489a0fe19911
BalashovaMa <balashova.m.nd@gmail.com>
2023-06-15 15:24:46 +0300

    feat: add 05-frontend-and-backend

---
+++ 12-express/05-frontend-and-backend/frontend/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [person, setPerson] = useState();
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000')
+      .then((res) => res.json())
+      .then((data) => setPerson(data))
+      .catch((err) => console.log(err))
+  }, [])
+  return (
+    <div className="App">
+      {person && (
+        <>
+          <h1>{person.name}</h1>
+          <h2>{person.isOwner ? 'Owner' : 'not Owner'}</h2>
+        </>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
